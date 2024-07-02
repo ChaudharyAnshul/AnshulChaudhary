66f30afb547d134dfd15c13aa28989c5d3d5d139
ChaudharyAnshul <chaudhary.ans@northeastern.edu>
2024-07-01 21:25:44 -0400

    router removed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './global.css';
 
 import { useScrollToTop } from './hooks/use-scroll-to-top';
 
-import Router from './routes/sections';
+import IndexPage from './pages/app';
 import ThemeProvider from './theme';
 
 // ----------------------------------------------------------------------
@@ -12,7 +12,7 @@ export default function App() {
 
   return (
     <ThemeProvider>
-      <Router />
+      <IndexPage />
     </ThemeProvider>
   );
 }
