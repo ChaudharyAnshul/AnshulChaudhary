07f484d8348db25076375c63c397dae977883a36
ChaudharyAnshul <chaudhary.ans@northeastern.edu>
2025-04-07 19:39:09 -0400

    updarted

--- src/sections/view/dashboard.js
+++ src/sections/view/dashboard.js
@@ -43,8 +43,8 @@ const timeline = [
   { id: '3', type: 'order1', title: 'JPMorgan Chase & Co.', subtitle: 'Started my career as a Software Engineer', time: "July 2021 - July 2023" },
   { id: '4', type: 'order1', title: 'Bachelors of Engineering, India', subtitle: 'Completion of Undergraduate Education', time: "June 2021" },
   { id: '5', type: 'order1', title: 'Research Publication', subtitle: {text: 'IEEE Paper',link:"https://ieeexplore.ieee.org/document/9456440"}, time: "June 2021" },
-  { id: '6', type: 'order1', title: 'ISTE-VESIT: Student Chapter', subtitle: 'Advanced to Chief Technical Officer position', time: "July 2020 - June 2021" },
-  { id: '7', type: 'order1', title: 'ISTE-VESIT: Student Chapter', subtitle: 'Served as Technical Officer on the Student Council', time: "September 2019 - July 2020" },
+  { id: '6', type: 'order1', title: 'TechnoEdge Learning Services', subtitle: 'Software Engineer - Part Time', time: "May 2020 - April 2021" },
+  { id: '7', type: 'order1', title: 'ISTE-VESIT: Student Chapter', subtitle: 'Advanced to Chief Technical Officer position', time: "September 2019 - June 2021" },
 ];
 
 // Project Diversity 
